085d103e7ce462a73cfc80aafca97b74fc108517
SneMasondo <snemasondo87@gmail.com>
2025-03-17 05:31:44 -0700

    intel staged

--- 0x00-hello_world/0-main.c
+++ 0x00-hello_world/0-main.c
@@ -1,8 +1,12 @@
 #include <stdio.h>
 
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
-    printf("Hello, World!\n");
-    return 0;
+    printf("Hello, Holberton\n");
+    return (0);
 }
-
