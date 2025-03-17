bb708763dcc3c56fcce4a4a092744febdc9e6be1
SneMasondo <snemasondo87@gmail.com>
2025-03-17 05:02:36 -0700

    name stage

--- 0x00-hello_world/3-main.c
+++ 0x00-hello_world/3-main.c
@@ -1,7 +1,12 @@
 #include <stdio.h>
 
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
-    printf("cisfun");
+    printf("Holberton School\n");
     return 0;
 }
