8427f10ec8ee7e97c5c40643af2e372efcbc8725
SneMasondo <snemasondo87@gmail.com>
2025-03-18 01:32:42 -0700

    101-print_comb4 staging

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,26 @@
+#include <unistd.h>
+
+int main(void)
+{
+    int i, j, k;
+
+    for (i = 0; i < 8; i++) /* i = first digit */
+    {
+        for (j = i + 1; j < 9; j++) /* j = second digit */
+        {
+            for (k = j + 1; k < 10; k++) /* k = third digit */
+            {
+                putchar(i + '0'); /* print first digit */
+                putchar(j + '0'); /* print second digit */
+                putchar(k + '0'); /* print third digit */
+                if (i != 7 || j != 8 || k != 9) /* check if it's not the last combination */
+                {
+                    putchar(','); /* print comma */
+                    putchar(' '); /* print space */
+                }
+            }
+        }
+    }
+    putchar('\n'); /* print newline after the last combination */
+    return (0);
+}
