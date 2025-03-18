4dc9b8ad2ad136c055a0f30576533456da8f3c33
SneMasondo <snemasondo87@gmail.com>
2025-03-18 00:36:39 -0700

    positive_or_negative staging

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,26 +1,30 @@
-#include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
+#include <stdio.h>
 
+/**
+ * main - Entry point
+ *
+ * Description: Assigns a random number to n and prints if it is
+ * positive, negative, or zero.
+ *
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
-    int n;
+	int n;
 
-    srand(time(0));  /* Initialize random number generator */
-    n = rand() - RAND_MAX / 2;  /* Generate a random number */
+	/* Seed random number generator */
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
 
-    if (n > 0)
-    {
-        printf("%d is positive\n", n);
-    }
-    else if (n == 0)
-    {
-        printf("%d is zero\n", n);
-    }
-    else
-    {
-        printf("%d is negative\n", n);
-    }
+	/* Check if n is positive, negative, or zero */
+	if (n > 0)
+		printf("%d is positive\n", n);
+	else if (n == 0)
+		printf("%d is zero\n", n);
+	else
+		printf("%d is negative\n", n);
 
-    return 0;
+	return (0);
 }
