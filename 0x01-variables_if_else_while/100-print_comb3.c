2ee4b9fe2215b379f059291dccb07086b01e6bb5
SneMasondo <snemasondo87@gmail.com>
2025-03-18 01:20:46 -0700

    100-print_comb3

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -7,26 +7,26 @@
  */
 int main(void)
 {
-    int num1, num2;
+	int num1, num2;
 
-    /* Loop for the first digit (num1) */
-    for (num1 = 0; num1 <= 8; num1++)
-    {
-        /* Loop for the second digit (num2) */
-        for (num2 = num1 + 1; num2 <= 9; num2++)
-        {
-            putchar(num1 + '0'); /* Print the first digit */
-            putchar(num2 + '0'); /* Print the second digit */
+	/* Loop for the first digit (num1) */
+	for (num1 = 0; num1 <= 8; num1++)
+	{
+		/* Loop for the second digit (num2) */
+		for (num2 = num1 + 1; num2 <= 9; num2++)
+		{
+			putchar(num1 + '0'); /* Print the first digit */
+			putchar(num2 + '0'); /* Print the second digit */
 
-            /* Print a comma and space unless it's the last combination */
-            if (num1 != 8 || num2 != 9)
-            {
-                putchar(','); 
-                putchar(' ');
-            }
-        }
-    }
+			/* Print a comma and space unless it's the last combination */
+			if (num1 != 8 || num2 != 9)
+			{
+				putchar(','); 
+				putchar(' ');
+			}
+		}
+	}
 
-    putchar('\n'); /* Print a new line */
-    return (0);
+	putchar('\n'); /* Print a new line */
+	return (0);
 }
