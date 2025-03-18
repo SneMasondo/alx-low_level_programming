f7aaa69232837b428fe7294a65f5456743d1d5ba
SneMasondo <snemasondo87@gmail.com>
2025-03-18 01:38:28 -0700

    101-print_comb4 staging

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -1,26 +1,38 @@
-#include <stdio.h>  /* Add the stdio.h header for putchar */
+#include <stdio.h> /* Add the stdio.h header for putchar */
 
+/**
+ * main - prints all possible different combinations of three digits
+ *
+ * Description: The program generates and prints all possible combinations
+ * of three different digits in ascending order, separated by commas and 
+ * a space. The digits are printed in increasing order, and no duplicate 
+ * combinations are printed.
+ *
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
     int i, j, k;
 
-    for (i = 0; i < 8; i++) /* i = first digit */
+    for (i = 0; i < 8; i++)  /* i = first digit */
     {
-        for (j = i + 1; j < 9; j++) /* j = second digit */
+        for (j = i + 1; j < 9; j++)  /* j = second digit */
         {
-            for (k = j + 1; k < 10; k++) /* k = third digit */
+            for (k = j + 1; k < 10; k++)  /* k = third digit */
             {
-                putchar(i + '0'); /* print first digit */
-                putchar(j + '0'); /* print second digit */
-                putchar(k + '0'); /* print third digit */
-                if (i != 7 || j != 8 || k != 9) /* check if it's not the last combination */
+                putchar(i + '0');  /* print first digit */
+                putchar(j + '0');  /* print second digit */
+                putchar(k + '0');  /* print third digit */
+
+                if (i != 7 || j != 8 || k != 9)  /* check if it's not the last combination */
                 {
-                    putchar(','); /* print comma */
-                    putchar(' '); /* print space */
+                    putchar(',');  /* print comma */
+                    putchar(' ');  /* print space */
                 }
             }
         }
     }
-    putchar('\n'); /* print newline after the last combination */
+    putchar('\n');  /* print newline after the last combination */
+
     return (0);
 }
