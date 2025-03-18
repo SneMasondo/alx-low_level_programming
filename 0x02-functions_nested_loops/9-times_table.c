4ef66b67802b400070f360974842fb48ade89d39
SneMasondo <snemasondo87@gmail.com>
2025-03-18 05:27:16 -0700

    9-times_table.c staging

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -5,32 +5,32 @@
  */
 void times_table(void)
 {
-    int i, j, product;
+	int i, j, product;
 
-    for (i = 0; i <= 9; i++)
-    {
-        for (j = 0; j <= 9; j++)
-        {
-            product = i * j;
+	for (i = 0; i <= 9; i++)
+	{
+		for (j = 0; j <= 9; j++)
+		{
+			product = i * j;
 
-            /* Print first value without leading space */
-            if (j == 0)
-                _putchar('0' + product);
-            else
-            {
-                _putchar(',');
-                _putchar(' ');
+			/* Print first value without leading space */
+			if (j == 0)
+				_putchar('0' + product);
+			else
+			{
+				_putchar(',');
+				_putchar(' ');
 
-                /* Add extra space for alignment if product < 10 */
-                if (product < 10)
-                    _putchar(' ');
+				/* Add extra space for alignment if product < 10 */
+				if (product < 10)
+					_putchar(' ');
 
-                /* Print the product */
-                if (product >= 10)
-                    _putchar('0' + (product / 10));
-                _putchar('0' + (product % 10));
-            }
-        }
-        _putchar('\n');
-    }
+				/* Print the product */
+				if (product >= 10)
+					_putchar('0' + (product / 10));
+				_putchar('0' + (product % 10));
+			}
+		}
+		_putchar('\n');
+	}
 }
