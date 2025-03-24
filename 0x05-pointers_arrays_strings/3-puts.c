2b18467559525d17b6474a45d8116cbb05543ec4
SneMasondo <snemasondo87@gmail.com>
2025-03-24 06:38:06 -0700

    3-puts.c staging

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -1,13 +1,30 @@
 #include <unistd.h>
 
 /**
- * _putchar - writes the character c to stdout
- * @c: The character to print
+ * _putchar - Writes a character to stdout.
+ * @c: The character to be printed.
  *
- * Return: On success 1.
- * On error, -1 is returned, and errno is set appropriately.
+ * Return: On success, returns 1. On error, returns -1 and sets errno appropriately.
  */
-int _putchar(char c)
+void _putchar(char c)
 {
-    return (write(1, &c, 1));
+    write(1, &c, 1);
+}
+
+/**
+ * _puts - Prints a string followed by a new line to stdout.
+ * @str: Pointer to the string to be printed.
+ *
+ * Description: This function loops through each character of the string
+ * and prints it using the _putchar function until the null-terminator is reached.
+ * A new line is printed at the end.
+ */
+void _puts(char *str)
+{
+    while (*str)
+    {
+        _putchar(*str);
+        str++;
+    }
+    _putchar('\n');
 }
