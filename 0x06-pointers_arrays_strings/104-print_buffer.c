fd79570bce4f74cc28aa7078176035f7a1084902
SneMasondo <snemasondo87@gmail.com>
2025-03-31 00:51:53 -0700

    104-print_buffer.c staging

--- 0x06-pointers_arrays_strings/104-print_buffer.c
+++ 0x06-pointers_arrays_strings/104-print_buffer.c
@@ -8,31 +8,31 @@
  */
 void print_buffer(char *b, int size)
 {
-	int i, j;
+    int i, j;
 
-	if (size <= 0)
-	{
-		printf("\n");
-		return;
-	}
+    if (size <= 0)
+    {
+        printf("\n");
+        return;
+    }
 
-	for (i = 0; i < size; i += 10)
-	{
-		printf("%08x: ", i); /* Print the address in hexadecimal */
-		for (j = i; j < i + 10; j++)
-		{
-			if (j < size)
-				printf("%02x ", b[j] & 0xFF); /* Print the hex value of the byte */
-			else
-				printf("   "); /* Print spaces if there are less than 10 bytes remaining */
-		}
-		for (j = i; j < i + 10 && j < size; j++)
-		{
-			if (b[j] >= 32 && b[j] <= 126)
-				printf("%c", b[j]); /* Print printable character */
-			else
-				printf("."); /* Print dot for non-printable characters */
-		}
-		printf("\n"); /* New line after each row */
-	}
+    for (i = 0; i < size; i += 10)
+    {
+        printf("%08x: ", i); /* Print the address in hexadecimal */
+        for (j = i; j < i + 10; j++)
+        {
+            if (j < size)
+                printf("%02x ", b[j] & 0xFF); /* Print the hex value of the byte */
+            else
+                printf("   "); /* Print spaces if there are less than 10 bytes remaining */
+        }
+        for (j = i; j < i + 10 && j < size; j++)
+        {
+            if (b[j] >= 32 && b[j] <= 126)
+                printf("%c", b[j]); /* Print printable character */
+            else
+                printf("."); /* Print dot for non-printable characters */
+        }
+        printf("\n"); /* New line after each row */
+    }
 }
