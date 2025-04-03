868516188aa0270ed37bbd904ea06e7917e5579a
SneMasondo <snemasondo87@gmail.com>
2025-04-03 00:24:12 -0700

    101-strtow.c staging

--- 0x0B-malloc_free/101-strtow.c
+++ 0x0B-malloc_free/101-strtow.c
@@ -22,6 +22,30 @@ int count_words(char *str)
 }
 
 /**
+ * allocate_word - Allocates memory and copies a word from the string.
+ * @str: The string containing the word.
+ * @start: The start index of the word.
+ * @length: The length of the word.
+ *
+ * Return: A pointer to the allocated word.
+ */
+char *allocate_word(char *str, int start, int length)
+{
+	char *word;
+	int i;
+
+	word = malloc(sizeof(char) * (length + 1));
+	if (word == NULL)
+		return (NULL);
+
+	for (i = 0; i < length; i++)
+		word[i] = str[start + i];
+	word[i] = '\0';
+
+	return (word);
+}
+
+/**
  * strtow - Splits a string into words.
  * @str: The string to split.
  *
@@ -30,57 +54,51 @@ int count_words(char *str)
 char **strtow(char *str)
 {
 	char **words;
-	int i, j, k, word_count = 0, word_len = 0;
+	int i, k = 0, word_count, word_len = 0;
 
-	/* Return NULL if the input string is NULL or empty */
 	if (str == NULL || str[0] == '\0')
+		return (NULL);
+
+	for (i = 0; str[i]; i++)
+	{
+		if (str[i] != ' ')
+			break;
+	}
+	if (str[i] == '\0')
 		return (NULL);
 
 	word_count = count_words(str);
 
-	/* Allocate memory for the array of words */
 	words = malloc(sizeof(char *) * (word_count + 1));
 	if (words == NULL)
 		return (NULL);
 
-	/* Split the string into words */
 	i = 0;
-	k = 0;
 	while (str[i] && k < word_count)
 	{
-		/* Skip leading spaces */
 		if (str[i] == ' ')
 		{
 			i++;
 			continue;
 		}
 
-		/* Count the length of the current word */
 		word_len = 0;
 		while (str[i + word_len] && str[i + word_len] != ' ')
 			word_len++;
 
-		/* Allocate memory for the current word and copy it */
-		words[k] = malloc(sizeof(char) * (word_len + 1));
+		words[k] = allocate_word(str, i, word_len);
 		if (words[k] == NULL)
 		{
-			for (j = 0; j < k; j++)
+			for (int j = 0; j < k; j++)
 				free(words[j]);
 			free(words);
 			return (NULL);
 		}
 
-		/* Copy the word */
-		for (j = 0; j < word_len; j++)
-			words[k][j] = str[i + j];
-		words[k][j] = '\0';
-
-		/* Move to the next word */
 		i += word_len;
 		k++;
 	}
 
-	/* Null-terminate the array of words */
 	words[k] = NULL;
 
 	return (words);
