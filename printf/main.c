4f65e09eaad49f808aa59b7b5b770f8151ba9a8a
SneMasondo <snemasondo87@gmail.com>
2025-04-03 05:27:57 -0700

    printf staging

--- printf/main.c
+++ printf/main.c
@@ -1,10 +1,11 @@
 #include "main.h"
 
-int main(void)
-{
-	_printf("Character: %c\n", 'H');
-	_printf("String: %s\n", "Hello, world!");
-	_printf("Percent sign: %%\n");
-	return (0);
+int main(void) {
+    int len1 = _printf("Character: %c\n", 'A');
+    int len2 = _printf("String: %s\n", "Hello, world!");
+    int len3 = _printf("Percent sign: %%\n");
+
+    _printf("Printed %d characters in total.\n", len1 + len2 + len3);
+    return 0;
 }
 
---
+++ printf/printf.c
@@ -0,0 +1,48 @@
+#include <stdarg.h>
+#include <unistd.h>
+#include "main.h"
+
+int _putchar(char c) {
+    return write(1, &c, 1);
+}
+
+int _printf(const char *format, ...) {
+    int count = 0;
+    va_list args;
+    const char *ptr;
+
+    va_start(args, format);
+
+    for (ptr = format; *ptr != '\0'; ptr++) {
+        if (*ptr == '%') {
+            ptr++;  // Move to the specifier
+            if (*ptr == 'c') {  // Handle %c
+                char c = va_arg(args, int);  // %c is promoted to int
+                _putchar(c);
+                count++;
+            } else if (*ptr == 's') {  // Handle %s
+                char *str = va_arg(args, char *);
+                while (*str) {
+                    _putchar(*str);
+                    str++;
+                    count++;
+                }
+            } else if (*ptr == '%') {  // Handle %%
+                _putchar('%');
+                count++;
+            } else {
+                // If the specifier is not handled, print the % symbol and the next character
+                _putchar('%');
+                _putchar(*ptr);
+                count += 2;
+            }
+        } else {
+            _putchar(*ptr);  // Print regular characters
+            count++;
+        }
+    }
+
+    va_end(args);
+    return count;
+}
+
